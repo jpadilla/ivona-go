905fc80a5fb88947b26a4bbe144dfff57f51f5e7
José Padilla <jpadilla@webapplicate.com>
2014-09-25 10:02:29 -0400

    Add examples for docs

---
+++ example_test.go
@@ -0,0 +1,21 @@
+package ivona_test
+
+import (
+	"log"
+
+	ivona "github.com/jpadilla/ivona-go"
+)
+
+func ExampleIvona_CreateSpeech() {
+	client := ivona.New("IVONA_ACCESS_KEY", "IVONA_SECRET_KEY")
+	options := ivona.NewSpeechOptions("Hello World")
+	r, err := client.CreateSpeech(options)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%v\n", len(r.Audio))
+	log.Printf("%v\n", r.ContentType)
+	log.Printf("%v\n", r.RequestID)
+}
