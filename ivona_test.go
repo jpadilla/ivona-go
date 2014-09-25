d8431da90f7e3d91107666e0dcb2e655b8acd80a
José Padilla <jpadilla@webapplicate.com>
2014-09-25 09:55:19 -0400

    Add tests

---
+++ ivona_test.go
@@ -0,0 +1,42 @@
+package ivona_test
+
+import (
+	"os"
+	"testing"
+
+	ivona "github.com/jpadilla/ivona-go"
+)
+
+var (
+	ivonaAccessKey = os.Getenv("IVONA_ACCESS_KEY")
+	ivonaSecretKey = os.Getenv("IVONA_SECRET_KEY")
+	testText       = "Hello World"
+)
+
+func init() {
+	if len(ivonaAccessKey) == 0 || len(ivonaSecretKey) == 0 {
+		panic("IVONA_ACCESS_KEY and IVONA_SECRET_KEY environment variables are needed to run tests!\n")
+	}
+}
+
+func TestIvona_CreateSpeech(t *testing.T) {
+	client := ivona.New(ivonaAccessKey, ivonaSecretKey)
+	options := ivona.NewSpeechOptions(testText)
+	r, err := client.CreateSpeech(options)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	audioLength := len(r.Audio)
+	expectedAudioLength := 6314
+	expectedContentType := "audio/mpeg"
+
+	if r.ContentType != expectedContentType {
+		t.Errorf("ContentType %v does not match", r.ContentType)
+	}
+
+	if audioLength != expectedAudioLength {
+		t.Errorf("Audio length %v does not match", audioLength)
+	}
+}
